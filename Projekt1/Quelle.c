ce53c94e5e4188bc17f1c513d2fea87dd21055dc
Heiner Büscher <heiner@buescher.de>
2018-08-08 19:49:05 +0200

    initialer Commit

---
+++ Projekt1/Quelle.c
@@ -0,0 +1,26 @@
+/*
+* Heiner Büscher, Steffen Tietzel
+* Programm zur parallelisierung des Job-Shop Problems
+*
+*
+*
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <mpi.h>
+
+
+int main(int argc, char **argv) {
+
+
+	int MPI_Init(int * argc, char *** argv);
+
+
+
+
+
+
+
+}
