eced82bd5fa8ba7fde4f6863ba2edf76efc7eac1
Heiner Büscher <heiner@buescher.de>
2018-08-08 20:48:35 +0200

    fopen hinzugefügt

---
+++ Projekt1/Projekt1/Quelle.cpp
@@ -0,0 +1,44 @@
+/*
+* Heiner Büscher, Steffen Tietzel
+* Programm zur parallelisierung des Job-Shop Problems
+*
+*
+*
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <mpi.h>
+
+
+int main(int argc, char **argv) {
+
+	int facilities;
+	int operations;
+
+
+
+
+	int MPI_Init(int * argc, char *** argv);
+
+
+
+	FILE *fp;
+
+	fp = fopen("jobs.txt", "r");
+
+	if (fp == NULL) {
+
+		printf("Datei konnte nicht geoeffnet werden!\n");
+	}
+	else {
+
+		//TODO
+
+		fclose(fp);
+	}
+
+
+
+}
