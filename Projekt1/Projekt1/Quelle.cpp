50c0ceb77646be6e1093db5e035c926566d97802
Heiner Büscher <heiner@buescher.de>
2018-08-17 22:26:39 +0200

    Einlesen der Textdatei

--- Projekt1/Projekt1/Quelle.cpp
+++ Projekt1/Projekt1/Quelle.cpp
@@ -10,14 +10,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <vector>
+#include <string.h>
 
 
-void calcDimensions(FILE* fp, int& row, int& maxcolumns, int& maxDigits) {
+/*void calcDimensions(FILE* fp, int& row, int& maxcolumns, int& maxDigits) {
 	int ch = 0;
 	int digits = 0;
 	int columns = 1;
 
-	row++;
 	while ((ch = fgetc(fp)) != EOF)
 	{
 		if (ch == '\n')
@@ -39,34 +40,46 @@ void calcDimensions(FILE* fp, int& row, int& maxcolumns, int& maxDigits) {
 			digits++;
 		}
 	}
-}
+}*/
 
-/*int calcDimensions(FILE* fp, int* size) {
+void readjobs(FILE* fp, int** array,int linesToRead, int facilityCount) {
 	int ch = 0;
-	int i = 0;
+	int rows = 0;
+	while ((ch = fgetc(fp)) != '\n')
+	{
+		rows = ch - 48;
+		//rows = rows * 10 + (ch - 48);
+	}
 
-	int m = 0;
-	int n = 0;
+	if (rows > linesToRead) rows = linesToRead;
 
+	array = (int**)malloc(rows * sizeof(int*));
+
+	for (int i = 0; i < rows; i++) {
+		/* size_y is the height */
+		array[i] = (int*)malloc(facilityCount * sizeof(int));
+	}
+	int i = 0, j = 0;
 	while ((ch = fgetc(fp)) != EOF)
 	{
 		if (ch == '\n')
 		{
-
+			i++;
+			printf("\n");
 		}
 		else if (ch == ' ')
 		{
-
-
+			j++;
+			printf(" ");
 		}
-		else
+		else	//wen es ne nummer ist
 		{
-			num[i] = ch;
-			i++;
+			array[i][j] = ch - 48;
+			printf("%i", array[i][j]);
+			//array[i][j] = array[i][j] * 10 + (ch - 48);
 		}
 	}
 }
-*/
 
 int main(int argc, char** argv) {
 
@@ -74,7 +87,6 @@ int main(int argc, char** argv) {
 	int jobs_count=-1;
 
 	const int facilities[] = {1};
-
 
 	MPI_Init(&argc, &argv);
 
@@ -91,22 +103,24 @@ int main(int argc, char** argv) {
 	//hier kommt noch Code hin, der bestimmt ganz Toll sein wird
 
 
-
+	int** jobs = NULL;
 	FILE *fp;
 	fp = fopen("jobs.txt", "r");
 
-	const int jobs[] = { 1 };
+	
 	if (fp == NULL) {
 		printf("Datei konnte nicht geoeffnet werden!!\n");
 	}
 	else {
 		printf("Datei ist lesbar\n");
-		int jobRows = 0;
-		int jobColums = 0;
-		int jobMaxDigits = 0;
-		calcDimensions(fp, jobRows, jobColums, jobMaxDigits);
-		printf("%i, %i, %i", jobRows, jobColums, jobMaxDigits);
-		
+		readjobs(fp, jobs, 6, 6);
+		printf("\n\n%i", jobs[5][5]);
+		/*for (int i = 0; i < 6; i++) {
+			printf("\n");
+			for (int j = 0; j < 6; j++) {
+				printf("%d ", jobs[i][j]);
+			}
+		}*/
 		fclose(fp);
 	}
 
